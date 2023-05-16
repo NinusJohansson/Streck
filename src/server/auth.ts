59cbed3a5f6d37a84cd3d4a2a1d2e7cdb9ae2804
Ninusj <128705752+Ninusj@users.noreply.github.com>
2023-05-16 18:28:24 +0200

    då

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -58,14 +58,14 @@ export const authOptions: NextAuthOptions = {
     //   clientSecret: process.env.GOOGLE_CLIENT_SECRET
     // })
     // /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    //  * ...add more providers here.
+    //  *
+    //  * Most other providers require a bit more work than the Discord provider. For example, the
+    //  * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+    //  * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+    //  *
+    //  * @see https://next-auth.js.org/providers/github
+    //  */
   ],
 };
 
