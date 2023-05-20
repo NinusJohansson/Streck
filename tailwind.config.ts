5501572f598546aeac3a5c4134dc4462dc75fbaf
Ninusj <128705752+Ninusj@users.noreply.github.com>
2023-05-21 00:02:01 +0200

    Font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cooper: ['COOPER', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
