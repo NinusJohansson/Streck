e7f173b3fb6b68398389754e576fecbf5a48c93c
Ninusj <128705752+Ninusj@users.noreply.github.com>
2023-07-10 00:54:46 +0200

    hej 3

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -36,9 +36,9 @@ export function SideNav() {
           <li>
             <button onClick={() => void signIn()}>
               <IconHoverEffect>
-                <span className="flex items-center gap-4">
+                <span className="flex-col items-center gap-4">
                   <VscSignIn className="h-8 w-8 fill-green-700" />
-                  <span className="hidden text-lg text-green-700 md:inline">
+                  <span className="inline text-lg text-green-700">
                     Log In
                   </span>
                 </span>
@@ -51,7 +51,7 @@ export function SideNav() {
               <IconHoverEffect>
                 <span className="flex-col items-center gap-4">
                   <VscSignOut className="h-8 w-8 fill-red-700" />
-                  <span className="hidden text-lg text-red-700 md:inline">
+                  <span className="inline text-lg text-red-700">
                     Log Out
                   </span>
                 </span>
