1d9bf198a685cfd4d4d67bc2d04c9546ac336235
Ninusj <128705752+Ninusj@users.noreply.github.com>
2023-07-10 00:53:17 +0200

    hej 3

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -49,7 +49,7 @@ export function SideNav() {
           <li>
             <button onClick={() => void signOut()}>
               <IconHoverEffect>
-                <span className="flex items-center gap-4">
+                <span className="flex-col items-center gap-4">
                   <VscSignOut className="h-8 w-8 fill-red-700" />
                   <span className="hidden text-lg text-red-700 md:inline">
                     Log Out
